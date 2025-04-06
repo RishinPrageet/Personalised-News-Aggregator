fa94a93222689eaabf3f01d0fa3b70b0a167dcbc
Rishin Prageet <kira70218@gmail.com>
2025-04-07 00:42:55 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import uvicorn
+import warnings
+
+warnings.filterwarnings("ignore")
 if __name__ == "__main__":
-    uvicorn.run("backend.main:app", host="localhost", port=9000, ssl_keyfile="key.pem", ssl_certfile="cert.pem",reload=True)
+    uvicorn.run("backend.main:app", host="localhost", port=8000)
   
